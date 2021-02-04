47137fdbfbaf157fa2216e65840ba5fa56877344
Zaahir Dawood <zaahirdawood@zaahirs-mbp.home>
2021-02-04 18:42:42 +0000

    Initial data loading and column removal and created new file.

--- Netflix.R
+++ Netflix.R
@@ -1,12 +1,24 @@
 
 
-
 # 1. Import dataset
 
-Netflix_r <– read.csv()
-Netflix_z <– read.csv()
+netflix_version1 <-  read.csv("netflix_titles.csv")
 
-# 2. 
+#>>> We must still add an additional dataset
+
+# 2. Remove Unwanted Columns
+
+## 2.1 Checking the variable names and dataset dimensions
+colnames(netflix_version1)
+dim(netflix_version1)
+
+## 2.2 Removing the agreed columns
+netflix_version2 <-  netflix_version1[,-c(5,10)]
+
+## 2.3 Checking the variable names and dataset dimensions to confirm changes
+colnames(netflix_version2)
+dim(netflix_version2)
+
 
 
 
