e8f874992183b34810b688d81877263e68c55119
Zaahir Dawood <zaahirdawood@Zaahirs-MacBook-Pro.local>
2021-02-11 17:27:27 +0000

    Removed Columns and created version5 of the dataset

--- Netflix.R
+++ Netflix.R
@@ -1,18 +1,18 @@
 ### Libraries ###
 
 
-if(require(tidyverse,RCurl) == FALSE)
+if(require(tidyverse) == FALSE)
 		
 	{
 		
 		# Packages are installed if the statement is false
-		install.packages('tidyverse')
-		install.packages('RCurl')
+		#install.packages('tidyverse')
 		
 		# Loading libraries
-		library(tidyverse,RCurl)
+		library(tidyverse)
 	}
 	
+
 #----------------------------------------------------------------------------------------------------------------------------------------------------
 #(Zaahir- Processing First Dataset)
 # 1. Import dataset
@@ -35,29 +35,63 @@ colnames(netflix_version2)
 dim(netflix_version2)
 
 
-
-
 #--------------------------------------------------------------------------------------------------------------------------------------------------
 #(Zaahir- joining data)
 
-
-library(dplyr)
-netflix_version2
 
 
 imdb_rating <- read.csv("mycsvfile.csv")
 
 netflix_version3 <- netflix_version2 %>% 
-left_join(imdb_rating, by ="title",suffix =c("_orginal","imdb")) 
-# write_rds(netflix_version3,file = "netflix_version3.rda")
+left_join(imdb_rating, by ="title",suffix =c("_orginal","_imdb")) 
 # write.csv(netflix_version3,file= "netflix_version3.csv")
 
 
 netflix_version4 <- netflix_version2 %>% 
-inner_join(imdb_rating, by ="title",suffix =c("_orginal","imdb")) 
-# write_rds(netflix_version4,file= "netflix_version4.rda")
+inner_join(imdb_rating, by ="title",suffix =c("_orginal","_imdb")) 
 # write.csv(netflix_version4,file= "netflix_version4.csv")
 
 
 sum(is.na(netflix_version3))
 sum(is.na(netflix_version4))
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Joining Netflix Original Columns
+
+netflix_originals <- read.csv("netflix_originals.csv")
+netflix_version4 <- read.csv("netflix_version4.csv")
+
+# Removing unncessary columns from both datasets to enable a merge.
+
+## Checking colnames in netflix_originals
+
+colnames(netflix_originals)
+colnames(netflix_version4)
+
+head(netflix_version4)
+
+netflix_version4 <- netflix_version4[,2:22]
+View(netflix_version4)
+
+netflix_version4_removed_columns  <- netflix_version4 %>% 
+  select(
+    show_id_imdb,
+    title,
+    type_orginal,
+    title,
+    director_orginal,
+    country_orginal,
+    date_added_orginal,
+    release_year_orginal,
+    listed_in_orginal,
+    description_orginal,
+    duration,
+    rating_imdb
+  )
+
+View(netflix_version4_removed_columns)
+
+
+
