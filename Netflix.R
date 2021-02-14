e0795b1b9f840cb1c131d46911d1b7d8096bbe10
Zaahir Dawood <zaahirdawood@zaahirs-mbp.home>
2021-02-14 22:21:08 +0000

    Commented out some lines and added some notes.

--- Netflix.R
+++ Netflix.R
@@ -6,11 +6,11 @@ if(require(tidyverse,visdat) == FALSE)
 	{
 		
 		# Packages are installed if the statement is false
-		#install.packages('tidyverse')
+		install.packages('tidyverse')
     install.packages('visdat')
-		
-		# Loading libraries
-		library(tidyverse)
+    
+    # Install libraries
+    library(tidyverse)
 	}
 	
 
@@ -18,18 +18,18 @@ if(require(tidyverse,visdat) == FALSE)
 #(Zaahir- Processing First Dataset)
 # 1. Import dataset
 
-netflix_version1 <-  read.csv("netflix_titles.csv")
+# netflix_version1 <-  read.csv("netflix_titles.csv") # No longer need to load this we have version2
 
-#>>> We must still add an additional dataset
 
 # 2. Remove Unwanted Columns
 
 ## 2.1 Checking the variable names and dataset dimensions
-colnames(netflix_version1)
-dim(netflix_version1)
+# colnames(netflix_version1)
+# dim(netflix_version1)
 
+#>>>>> If you want to load netflix_version2 double click on it from the data folder.
 ## 2.2 Removing the agreed columns
-netflix_version2 <-  netflix_version1[,-c(5,10)]
+# netflix_version2 <-  netflix_version1[,-c(5,10)]
 
 ## 2.3 Checking the variable names and dataset dimensions to confirm changes
 colnames(netflix_version2)
@@ -43,17 +43,15 @@ dim(netflix_version2)
 
 imdb_rating <- read.csv("mycsvfile.csv")
 
-netflix_version3 <- netflix_version2 %>% 
-left_join(imdb_rating, by ="title",suffix =c("_orginal","_imdb")) 
+# netflix_version3 <- netflix_version2 %>% 
+# left_join(imdb_rating, by ="title",suffix =c("_orginal","_imdb")) # generates 17,000 NA values
 # write.csv(netflix_version3,file= "netflix_version3.csv")
-
+# sum(is.na(netflix_version3)) # Test the number of NA's produced
 
 netflix_version4 <- netflix_version2 %>% 
 inner_join(imdb_rating, by ="title",suffix =c("_orginal","_imdb")) 
-# write.csv(netflix_version4,file= "netflix_version4.csv")
-
-
-sum(is.na(netflix_version3))
+# write.csv(netflix_version4,file= "netflix_version4.csv") 
+#>>> note:- Write this as a csv file if you need to.
 sum(is.na(netflix_version4))
 
 
@@ -64,7 +62,7 @@ sum(is.na(netflix_version4))
 netflix_originals <- read.csv("netflix_originals.csv")
 netflix_version4 <- read.csv("netflix_version4.csv")
 
-# Removing unncessary columns from both datasets to enable a merge.
+#>>> note:-  Removing unncessary columns from both datasets to enable a merge.
 
 ## Checking colnames in netflix_originals
 
@@ -74,7 +72,7 @@ colnames(netflix_version4)
 # Removing the extra first column
 
 netflix_version4 <- netflix_version4[,2:22]
-View(netflix_version4)
+# View(netflix_version4) #>>> note:- Use if you would like to inspect datset.
 
 # Removing the unnecessary columns
 netflix_version4_removed_columns  <- netflix_version4 %>% 
@@ -96,12 +94,16 @@ netflix_version4_removed_columns  <- netflix_version4 %>%
 
 # View(netflix_version4_removed_columns) #use this code only if you want to view results
 
-#write_csv(netflix_version4_removed_columns,file="netflix_version5.csv") #Use this code if you want to save anything to your local machine  
+#write_csv(netflix_version4_removed_columns,file="netflix_version5.csv") 
+#>>> note:- Use this code if you want to save anything to your local machine  
 
 ## Joining the Netflix Originals Dataset Columns to ours
 new_netflix_version4_removed_columns <- netflix_version4_removed_columns %>% 
   left_join(netflix_originals, by=c("title"="Title"),suffix =c("_previous","_originals")) 
 
+# new_netflix_version4_removed_columns <- netflix_version4_removed_columns %>% 
+#   inner_join(netflix_originals, by=c("title"="Title"),suffix =c("_previous","_originals")) 
+#>>> note:- The line of code above produces no NA's but eliminates 75% of the original data size.
 ## Checking the column names 
 new_netflix_version4_removed_columns %>% 
   colnames()
@@ -125,11 +127,12 @@ netflix_version6 <- new_netflix_version4_removed_columns %>%
     Genre
   )
 
-write_csv(netflix_version6,file="netflix_version6.csv")
+# write_csv(netflix_version6,file="netflix_version6.csv") # use this to produce the latest version of the 
+                                                          # dataset
 
 ## The number of NA's produced. 
 
-sum(is.na(test))
+sum(is.na(netflix_version6))
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -138,6 +141,7 @@ sum(is.na(test))
 
 visdat::vis_miss(netflix_version6) #Produces a visualisation of missing data in added columns
 
+# netflix_version6[is.na(netflix_version6$Original.Network),] <- "not netflix original"
 
 
 
